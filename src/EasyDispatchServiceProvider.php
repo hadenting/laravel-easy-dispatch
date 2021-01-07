d6c34c431d2d364110a3ac51966fb65d49102382
gc_xuanku <gc_xuanku@gagc.tv>
2021-01-07 16:40:35 +0800

    <feat>增加服务提供者，提供发布配置文件功能

---
+++ src/EasyDispatchServiceProvider.php
@@ -0,0 +1,22 @@
+<?php
+
+
+namespace Hadenting\EasyDispatch;
+
+
+use Carbon\Laravel\ServiceProvider;
+
+class EasyDispatchServiceProvider extends ServiceProvider
+{
+
+    public function boot()
+    {
+        $configPath = __DIR__ . '/../config/easy_dispatch.php';
+        if (function_exists('config_path')) {
+            $publishPath = config_path('easy_dispatch.php');
+        } else {
+            $publishPath = base_path('config/easy_dispatch.php');
+        }
+        $this->publishes([$configPath => $publishPath], 'config');
+    }
+}
